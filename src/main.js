779332aceb3a08db9a202c8d037163c662fe8d69
Nico Ontaneda <nicole.ontaneda@epn.edu.ec>
2022-02-06 18:58:27 -0500

    Servidor historia estabilziado 6

--- src/main.js
+++ src/main.js
@@ -24,6 +24,15 @@ app.use(bodyParse.json());
 }
 );
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 const router = express.Router();
 
 app.use(cors());
