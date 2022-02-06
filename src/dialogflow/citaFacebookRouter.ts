ba4d4cfd4b61bf1f6b586d061f3deb75da806f3a
Nico Ontaneda <nicole.ontaneda@epn.edu.ec>
2022-02-06 18:48:52 -0500

    Servidor historia estabilziado 5

--- src/dialogflow/citaFacebookRouter.ts
+++ src/dialogflow/citaFacebookRouter.ts
@@ -1,6 +1,5 @@
 const Facebook=require('./citaFacebookController.js');
 module.exports = (router) =>{
-    
-    router.get('/webhook/', Facebook.facebookCitas);
-    router.post("/webhook/", Facebook.facebookwebhook);
+    router.get('/webhookget/', Facebook.facebookCitas);
+    router.post("/webhookPost/", Facebook.facebookwebhook);
 }
