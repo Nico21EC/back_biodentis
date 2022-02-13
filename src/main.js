0fe3179abdaff18318e76299908a92f4efa7315a
Nico Ontaneda <nicole.ontaneda@epn.edu.ec>
2022-02-13 02:51:51 -0500

    server 22

--- src/main.js
+++ src/main.js
@@ -98,10 +98,10 @@ app.get("/messenger", (req, res) => {
 
 app.get("/messenger/webhook", function (req, res) {
   console.log(req);
-  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  //res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  //res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Content-Type','application/json');
   if (
     req.query["hub.mode"] === "subscribe" &&
@@ -127,10 +127,10 @@ app.listen(port, () => {
 });
 
 app.post("/messenger/webhook/", function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  //res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  //res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Content-Type','application/json');
   var data = req.body;
   // Make sure this is a page subscription
