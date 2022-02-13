0adff70571e84495cba0c6d8cb5bf1ed6138c9f0
Nico Ontaneda <nicole.ontaneda@epn.edu.ec>
2022-02-13 03:17:16 -0500

    server 26

--- src/main.js
+++ src/main.js
@@ -200,6 +200,7 @@ const callSendAPI = async (messageData) => {
   )
   await axios.post(url, messageData)
     .then(function (response) {
+     console.log(res.data)
       if (response.status == 200) {
         var recipientId = response.data.recipient_id;
         var messageId = response.data.message_id;
