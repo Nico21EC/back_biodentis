7e8bb8fe59542a88b636bb1921e597f43ae78a02
Nico Ontaneda <nicole.ontaneda@epn.edu.ec>
2022-03-12 16:39:28 -0500

    1

--- src/dialogflow/citaFacebookController.js
+++ src/dialogflow/citaFacebookController.js
@@ -87,7 +87,7 @@ async function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
   var quickReply = message.quick_reply;
-
+  //se usa para imprimir
   if (isEcho) {
     handleEcho(messageId, appId, metadata);
     return;
