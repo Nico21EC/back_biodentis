655bb9f7486149caf4d4b1efd645d2c591cdf615
Nico Ontaneda <nicole.ontaneda@epn.edu.ec>
2022-05-01 17:46:09 -0500

    Editar reserva Facebook

--- src/dialogflow/citaFacebookController.js
+++ src/dialogflow/citaFacebookController.js
@@ -8,3 +8,16 @@ exports.citasFacebook = (req, res) => {
     res.status(200).send(pac);
   });
 };
+
+exports.reservaEditFacebook = (req, res) => {
+  EsquemaCitaFacebookModel.findOne({})
+  .where({fecha: req.params.fecha})
+  .exec((err, act) => {
+      if(err) {
+          console.log('Hubo un error en reserva Facebook');
+          return res.status(500).json({error: err.message}); //debes enviar una respuesta o llamar al manejador de errores (return next(err))
+      }
+      console.log(act);
+      return res.status(200).json(act); // en este ejemplo se envía el resultado
+  });
+}
--- src/dialogflow/citaFacebookRouter.ts
+++ src/dialogflow/citaFacebookRouter.ts
@@ -2,5 +2,6 @@ const Facebook=require('./citaFacebookController.js');
 
 module.exports = (router) =>{
     router.get('/citasFacebook', Facebook.citasFacebook);
+    router.get('/reservaEditFacebook/:fecha', Facebook.reservaEditFacebook);
     //router.post('/messenger/webhook', Facebook.postFacebook);
 }
